c0990f6df0c88c9b5a5ba1cf0c0ef06efb330f9c
Schnretzl <wrhendley@gmail.com>
2025-01-01 16:02:48 -0600

    Clean up and change references to Manager components

--- src/App.jsx
+++ src/App.jsx
@@ -2,69 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import './AppStyles.css';
 import CustomerManager from './components/Customers/CustomerManager';
-import ProductForm from './components/Products/ProductForm';
-import ProductList from './components/Products/ProductList';
-import OrderForm from './components/Orders/OrderForm';
-import OrderList from './components/Orders/OrderList';
-import NavigationBar from './components/NavigationBar';
+import ProductManager from './components/Products/ProductManager';
+import OrderManager from './components/Orders/OrderManager';
 import Home from './components/Home';
-import axios from 'axios';
+import NavigationBar from './components/NavigationBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const App = () => {
-    const [customers, setCustomers] = useState([]);
-    const [products, setProducts] = useState([]);
-    const [selectedProduct, setSelectedProduct] = useState(null);
-
-    const [orders, setOrders] = useState([]);
-    const [selectedOrder, setSelectedOrder] = useState(null);
-
-    useEffect(() => {
-        fetchCustomers();
-        // fetchProducts();
-        // fetchOrders();
-    }, []);
-
-    const fetchCustomers = async () => {
-        try {
-            const response = await axios.get(`http://127.0.0.1:5000/customers`);
-            setCustomers(response.data);
-        } catch (error) {
-            console.error('Error fetching customers:', error);
-        }
-    };
-
-    const fetchProducts = async () => {
-        try {
-            const response = await axios.get(`http://127.0.0.1:5000/products`);
-            setProducts(response.data);
-        } catch (error) {
-            console.error('Error fetching products:', error);
-        }
-    };
-
-    const fetchOrders = async () => {
-        try {
-            const response = await axios.get(`http://127.0.0.1:5000/orders`);
-            setOrders(response.data);
-        } catch (error) {
-            console.error('Error fetching orders:', error);
-        }
-    };
-
-    const handleCustomerUpdated = () => {
-        fetchCustomers();
-    };
-
-    const handleProductUpdated = () => {
-        fetchProducts();
-    };
-
-    const handleOrderUpdated = () => {
-        fetchOrders();
-    };
-
     return (
         <div className='app-container'>
             <NavigationBar />
@@ -72,10 +17,10 @@ const App = () => {
                 <Route path='/' element={<Home />} />
                 <Route path='/customers' element={<CustomerManager />} />
                 <Route path='/customers/:id' element={<CustomerManager />} />
-                <Route path='/products' element={<ProductForm selectedProductId={({ match }) => match.params.id} onProductUpdated={handleProductUpdated} onUpdateProductList={handleProductUpdated} onDeleteProduct={handleProductUpdated} />} />
-                <Route path='/products/:id' element={<ProductForm onProductUpdated={handleProductUpdated} />} />
-                <Route path='/orders' element={<OrderList />} />
-                <Route path='/orders/:id' element={<OrderForm onOrderUpdated={handleOrderUpdated} />} />
+                <Route path='/products' element={<ProductManager />} />
+                <Route path='/products/:id' element={<ProductManager />} />
+                <Route path='/orders' element={<OrderManager />} />
+                <Route path='/orders/:id' element={<OrderManager />} />
             </Routes>
         </div>
     )
